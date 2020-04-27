2d868d3d16593edbc755d9f6ac4d018b856fe6be
RyumiJang <21800621@handong.edu>
2020-04-27 22:24:36 +0900

    last

---
+++ books.c
@@ -0,0 +1,175 @@
+#include "books.h"
+#include <string.h>
+
+T_Record* Books[MAX_MEMBERS]; 
+int _count = 0;
+T_Record *ptr;
+
+int b_is_available(){
+    int i;
+    for(i=0; i<MAX_MEMBERS; i++){
+	#ifdef DEBUG
+		printf("[RECORD] DEBUGGING processing\n");
+	#endif
+        if(Books[i]==NULL) return 1;
+    }
+    return 0;
+}
+
+int b_first_available(){
+    int i;
+    for(i=0; i<MAX_MEMBERS; i++){
+        if(Books[i]==NULL) return i;
+	#ifdef DEBUG
+	printf("[RECORD] %d finding NULL for saving",i);
+	#endif
+	
+    }
+    return -1;
+}
+
+int b_count(){
+    #ifdef DEBUG
+	printf("[REOCRD] return count number of books\n");
+    #endif
+    return _count;
+}
+
+void b_update(T_Record* p, int pr,char* s,char* de){
+    p->price = pr;
+    strcpy(p->subject, s);
+    strcpy(p->department, de);
+#ifdef DEBUG
+	printf("[RECORD] update scuccess for \n price: %d\nsubject %s\ndepartment: %s\n",p->price,p->subject,p->department);
+#endif
+}
+
+
+void b_create(int n,char* bn, char* sn, int pr, char* sp,char* s,char* de,char*pw){
+    int index = b_first_available();
+    Books[index] = (T_Record*)malloc(sizeof(T_Record));
+    T_Record* p = Books[index];
+ 	p->no = n;
+ 	strcpy(p->book_name, bn);
+    strcpy(p->seller_name, sn);  
+    p->price = pr;
+    strcpy(p->seller_phone, sp);
+    strcpy(p->subject, s);
+    strcpy(p->department, de);
+    strcpy(p->password, pw);
+    _count++;
+#ifdef DEBUG
+	printf("[RECORD] CREATE SUCCESS, Bookname: %s, Bookprice: %d, BookSeller: %s\n",p->book_name,p->price,p->seller_name);
+
+#endif
+}
+
+T_Record* b_search_by_name(char* bn){
+    int i;
+    for(i=0; i<MAX_MEMBERS; i++){
+        if(Books[i] && strcmp(Books[i]->book_name, bn)==0) return Books[i];
+#ifdef DEBUG
+	printf("[RECORD] SUCCESS SEARCH, going %d books\n",i);
+#endif	
+    }
+    return NULL;
+}
+
+char* b_to_string(T_Record* p){
+    static char str[80];
+    #ifdef DEBUG
+	printf("[RECORD] record to string success\n");
+   #endif
+    sprintf(str, "[%d] %s / %d / %s / %s / %s", p->no,p->book_name,p->price, p->seller_phone,p->seller_name, p->subject);
+	return str;
+}
+
+void b_get_all(T_Record* a[]){
+    int i, c=0;
+    for(i=0; i<MAX_MEMBERS; i++){
+        if(Books[i]){
+            a[c]=Books[i];
+            c++;
+	    #ifdef DEBUG
+		printf("[RECORD] record is gettering %d\n",i);
+	    #endif
+        }
+    }
+}
+
+void b_delete(T_Record* p){
+		int i, index;
+		for(i=0; i<MAX_MEMBERS; i++)
+				if(Books[i]==p) {
+						index=i;
+						break;
+				}
+		free(p);
+		Books[index] = NULL;
+		_count--;
+
+#ifdef DEBUG
+	printf("[RECORD] record deleting");
+#endif
+}
+void b_sort(T_Record* a[]){
+	int i,j;
+	b_get_all(a);
+	int c = b_count();
+	for(i=0;i<c-1;i++){
+			for(j=0;j<c-i-1;j++){
+				#ifdef DEBUG
+				printf("find books j[%d]\n",j);
+				printf("string compare with i and j result : %d\n",strcmp(a[i]->book_name,Books[j]->book_name));
+				#endif
+					if(strcmp(a[j]->book_name,a[j+1]->book_name)>0){
+						ptr = a[j];
+						a[j] = a[j+1];
+						a[j+1] = ptr; 
+						#ifdef DEBUG
+						printf("[RECORD] (%d).%s -saved",i,a[j]->book_name);
+						#endif
+					}
+			}
+		}
+	}
+char* b_getbookname(T_Record* p){
+    return p->book_name;
+}
+
+char* b_getsellerphone(T_Record* p){
+    return p->seller_phone;
+}
+
+char* b_getsellername(T_Record* p){
+    return p-> seller_name;
+}
+
+char* b_getsubject(T_Record* p){
+    return p->subject;
+}
+
+char* b_getdepartment(T_Record* p){
+    return p->department;
+}
+
+int b_getprice(T_Record* p){
+#ifdef debug
+   printf("getting price");
+#endif
+    return p->price;
+}
+
+char* b_getpassword(T_Record* p){
+#ifdef debug
+   prinf("getting password");
+#endif
+   return p->password;
+}
+int b_getsno(T_Record* p){
+#ifdef debug 
+   printf("get no is now started");
+#endif
+    return p->no;
+}
+
---
+++ books.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_MEMBERS 100      // 저장할 수 있는 회원 레코드 최대 갯수
+
+// 회원 정보 레코드의 데이터 타입을 정의함
+typedef struct book_sell{
+    int no;
+    char book_name[30];
+	char seller_name[20];
+	int price;
+	char seller_phone[20];
+	char subject[20];
+	char department[20];
+	char password[20];
+
+} T_Record;
+typedef struct book_selled{
+	int num;
+	char s_book_name[30];
+	char seller[20];
+	int s_price;
+	char s_department[20];
+} S_Record;
+
+
+// 라이브러리 함수 원형을 선언함
+int b_is_available();      
+int b_first_available();    
+int b_count();              
+void b_create(int n,char* bn, char* sn, int pr, char* sp,char* s,char* de,char*pw); 
+T_Record* b_search_by_name(char* n);                
+void b_update(T_Record* p, int pr,char* s,char* pw); 
+void b_delete(T_Record* p);     
+void b_get_all(T_Record* a[]);  
+char* b_to_string(T_Record* p); 
+char* b_getbookname(T_Record* p);
+char* b_getsellername(T_Record* p);    
+int b_getprice(T_Record* p);
+char* b_getsellerphone(T_Record* p);
+char* b_getsubject(T_Record* p);
+char* b_getdepartment(T_Record* p);
+char * b_getpassword(T_Record *p); 
+int b_getsno(T_Record *p);
+void b_sort(T_Record *a[]);
